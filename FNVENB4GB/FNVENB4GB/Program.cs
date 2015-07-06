8068d0a6e82c6cd1fecfce531f61196f394dd390
Timoral <atimson123@gmail.com>
2015-07-06 19:46:28 +0100

    C# Executable to launch ENBInjector.exe and then fnv4gb.exe. The Injector will be automatically closed when Fallout NV is closed.
    C# Executable to launch ENBInjector.exe and then fnv4gb.exe. The
    Injector will be automatically closed when Fallout NV is closed.
    
    I created this quick console applicaton when I started using an enb and
    saw that two executables needed to be launched to run Fallout New Vegas
    with an ENB and a modern system. I created this application in c#.

---
+++ FNVENB4GB/FNVENB4GB/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FNVENB4GB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String enbExecutable = "ENBInjector.exe";
+            String fourgbExecutable = "fnv4gb.exe";
+
+            String enbProcess = "ENBInjector";
+            String falloutProcess = "FalloutNV";
+
+
+            Console.WriteLine("Opening " + enbExecutable);
+            Process.Start(enbExecutable);
+
+            Console.WriteLine("Opening " + fourgbExecutable);
+            Process.Start(fourgbExecutable);
+
+            Console.WriteLine("Waiting For 20 Seconds..");
+            Thread.Sleep(20000);
+
+            Process[] fallout;
+            fallout = Process.GetProcessesByName(falloutProcess);
+            Process[] enb = Process.GetProcessesByName(enbProcess);
+
+            while (fallout.Length > 0)
+            {
+                fallout = Process.GetProcessesByName(falloutProcess);
+                //Console.WriteLine(falloutProcess + " Still Open");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Closing " + fourgbExecutable);
+            enb[0].Close();
+            Application.Exit();
+        }
+    }
+}
